4618152f2ab0783ed374703be1de07b85908e5c3
alex <alexwojtak@hotmail.com>
2020-06-30 03:39:17 +0100

    Create initial page with example transpiled JSX

---
+++ todo.js
@@ -0,0 +1,8 @@
+/* const JSX = <div>Lorem</div>;
+
+ReactDOM.render(JSX, document.getElementById('target')); */
+
+"use strict";
+
+var JSX = React.createElement("div", null, "Lorem");
+ReactDOM.render(JSX, document.getElementById('target'));
